07af5d560dea3e2b4529c4dfbc818fe434cbd2b7
Satoru <you@example.com>
2022-12-26 18:41:14 +0900

    feat: add functions

--- lib/OsdCustomViewer.vue
+++ lib/OsdCustomViewer.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { onMounted, ref, watch } from "vue";
-import OpenSeadragon from "openseadragon";
+import OpenSeadragon, { Overlay } from "openseadragon";
 
-const title = ref<string>("");
-const attribution = ref<string>("");
+// const title = ref<string>("");
+// const attribution = ref<string>("");
 
 const viewer = ref<any>();
+
+const currentManifest = ref<string>("");
 
 const props = withDefaults(
   defineProps<{
@@ -15,6 +17,7 @@ const props = withDefaults(
     page?: number;
     backgroundColor?: string;
     prefixUrl?: string;
+    regions?: any;
   }>(),
   {
     height: 600,
@@ -23,6 +26,7 @@ const props = withDefaults(
     page: 1,
     backgroundColor: "black",
     prefixUrl: "./images/",
+    regions: [],
   }
 );
 
@@ -31,20 +35,30 @@ const emit = defineEmits<{
 }>();
 
 onMounted(async () => {
+  await init();
+});
+
+let canvases: any[] = [];
+let overlays: any = {};
+
+const init = async () => {
   const manifest = props.manifest;
 
   if (!manifest) {
     return;
   }
 
+  currentManifest.value = manifest;
+
   const tileSources: string[] = [];
 
   const res = await fetch(props.manifest);
-  const data: any = await res.json()
-  title.value = data.label;
-  attribution.value = data.attribution;
+  const data: any = await res.json();
 
-  const canvases = data.sequences[0].canvases;
+  // title.value = data.label;
+  // attribution.value = data.attribution;
+
+  canvases = data.sequences[0].canvases;
   for (const canvas of canvases) {
     tileSources.push(canvas.images[0].resource.service["@id"] + "/info.json");
   }
@@ -56,10 +70,88 @@ onMounted(async () => {
     tileSources,
   });
 
+  //オーバーレイを作成する
+  createOverlays();
+
+  //移動
+  move();
+
   viewer.value.addHandler("page", function (event: any) {
     emit("page", event.page + 1);
   });
-});
+};
+
+const createOverlays = () => {
+  overlays = {};
+
+  // canvas_id毎のxywhsを作成する
+  const regionsByCanvas: any = {};
+
+  for (const region of props.regions) {
+    const spl = region.split("#xywh=");
+
+    const canvas_id = spl[0];
+
+    if (!regionsByCanvas[canvas_id]) {
+      regionsByCanvas[canvas_id] = [];
+    }
+
+    regionsByCanvas[canvas_id].push(spl[1]);
+  }
+
+  for (const canvas_id in regionsByCanvas) {
+    let index = -1;
+    let fullWidth = -1;
+
+    for (let i = 0; i < canvases.length; i++) {
+      if (canvases[i]["@id"] === canvas_id) {
+        index = i;
+        fullWidth = canvases[i].width;
+        break;
+      }
+    }
+
+    const xywhs = regionsByCanvas[canvas_id];
+
+    const page = index + 1;
+    overlays[page] = [];
+
+    for (const xywh_str of xywhs) {
+      const xywh = xywh_str.split(",");
+
+      const x = Number(xywh[0]) / fullWidth;
+      const y = Number(xywh[1]) / fullWidth;
+      const width = Number(xywh[2]) / fullWidth;
+      const height = Number(xywh[3]) / fullWidth;
+
+      const overlay = {
+        x,
+        y,
+        width,
+        height,
+        className: "highlight",
+      };
+
+      overlays[page].push(overlay);
+    }
+  }
+};
+
+const move = () => {
+  const page = props.page;
+  viewer.value.goToPage(page - 1);
+
+  //当該ページのオーバーレイを追加
+  overlay();
+};
+
+const overlay = () => {
+  if (overlays[props.page]) {
+    for (const overlay of overlays[props.page]) {
+      viewer.value.addOverlay(overlay);
+    }
+  }
+};
 
 watch(
   props,
@@ -67,8 +159,17 @@ watch(
     if (!viewer.value) {
       return;
     }
-    const page = value.page;
-    viewer.value.goToPage(page - 1);
+
+    //マニフェストが変わったら再初期化
+    if (value.manifest !== currentManifest.value) {
+      viewer.value.destroy();
+      init();
+      return;
+    }
+
+    move();
+
+    // overlay()
   },
   { deep: true }
 );
@@ -80,3 +181,9 @@ watch(
     :style="`height: ${height}px; width: ${width}; background-color: ${backgroundColor};`"
   ></div>
 </template>
+<style>
+.highlight {
+  opacity: 0.3;
+  background-color: #e9dc51;
+}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,98 @@
 <script setup lang="ts">
-import OsdCustomViewer from '../lib/OsdCustomViewer.vue'
-import { onMounted, ref } from 'vue'
-import { useRoute } from 'vue-router'
-const page = ref<number>(1)
-const manifest = ref<string>("")
-const route = useRoute()
+import OsdCustomViewer from "../lib/OsdCustomViewer.vue";
+import { onMounted, ref } from "vue";
+import { useRoute } from "vue-router";
+const page = ref<number>(1);
+const manifest = ref<string>("");
+const route = useRoute();
 const updatePage = (value: number) => {
-  page.value = value
-}
+  page.value = value;
+};
 onMounted(() => {
-  manifest.value = route && route.query.manifest ? String(route.query.manifest) : "https://www.dl.ndl.go.jp/api/iiif/3437686/manifest.json"
-})
+  manifest.value =
+    route && route.query.manifest
+      ? String(route.query.manifest)
+      : "https://www.dl.ndl.go.jp/api/iiif/3437686/manifest.json";
+});
 
 const move = (value: number) => {
-  page.value += value
-}
+  page.value += value;
+};
+
+const regions = ref<string[]>([]);
+
+const ex01 = () => {
+  regions.value = [];
+  manifest.value = "https://www.dl.ndl.go.jp/api/iiif/3437686/manifest.json";
+  page.value = 1;
+};
+
+const ex02 = () => {
+  regions.value = [
+    "https://dl.ndl.go.jp/api/iiif/2567061/canvas/69#xywh=2028,644,1452,1992",
+    "https://dl.ndl.go.jp/api/iiif/2567061/canvas/69#xywh=544,620,1452,2000",
+    "https://dl.ndl.go.jp/api/iiif/2567061/canvas/91#xywh=470,686,1548,1991",
+  ];
+  manifest.value = "https://www.dl.ndl.go.jp/api/iiif/2567061/manifest.json";
+  page.value = 69;
+};
+
+const ex03 = () => {
+  regions.value = [
+    "https://dl.ndl.go.jp/api/iiif/2567061/canvas/69#xywh=2028,644,1452,1992",
+    "https://dl.ndl.go.jp/api/iiif/2567061/canvas/69#xywh=544,620,1452,2000",
+    "https://dl.ndl.go.jp/api/iiif/2567061/canvas/91#xywh=470,686,1548,1991",
+    // "https://dl.ndl.go.jp/api/iiif/2567061/canvas/91#xywh=470,686,1548,1991"
+  ];
+  manifest.value = "https://www.dl.ndl.go.jp/api/iiif/2567061/manifest.json";
+  page.value = 91;
+};
 </script>
 
 <template>
-  <p>Page: {{ page }}</p>
+  <div style="margin-bottom: 8px">
+    <h3>サンプル</h3>
 
-  <button @click="move(1)" style="margin: 8px;">+</button>
-  <button @click="move(-1)" style="margin: 8px;">-</button>
+    <button style="margin: 4px" @click="ex01">
+      デフォルト（校異源氏物語）
+    </button>
+    <button style="margin: 4px" @click="ex02">
+      画像の一部をハイライト（絵入源氏物語）
+    </button>
+    <button style="margin: 4px" @click="ex03">
+      他のページをハイライト（絵入源氏物語）
+    </button>
+  </div>
+
+  <div style="margin-bottom: 8px">
+    <h3>ページネーション</h3>
+
+    <p>Page: {{ page }}</p>
+
+    <h4>コンポーネントの外からページネーション</h4>
+
+    <button @click="move(1)" style="margin: 4px">+</button>
+    <button @click="move(-1)" style="margin: 4px">-</button>
+  </div>
 
   <OsdCustomViewer
     v-if="manifest"
     @page="updatePage"
     :manifest="manifest"
     :page="page"
+    :regions="regions"
   />
-  <p>
+
+  <div>
     <ul>
-      <li>This demo application processes manifest files for which the IIIF Presentation API v.2 and Image API are enabled.</li>
-      <li>本デモアプリケーションでは、IIIF Presentation API v.2 および Image API が有効なマニフェストファイルが処理対象です。</li>
-    </ul></p>
+      <li>
+        This demo application processes manifest files for which the IIIF
+        Presentation API v.2 and Image API are enabled.
+      </li>
+      <li>
+        本デモアプリケーションでは、IIIF Presentation API v.2 および Image API
+        が有効なマニフェストファイルが処理対象です。
+      </li>
+    </ul>
+  </div>
 </template>
